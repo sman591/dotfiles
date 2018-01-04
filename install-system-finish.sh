d7dac8c8917a3b496ad73390bd7775d103e688df
Stuart Olivera <stuart@stuartolivera.com>
2018-01-03 23:12:07 -0500

    Update default ruby & node versions

--- install-system-finish.sh
+++ install-system-finish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing setting up Ruby, this could take a while..."
-rbenv install 2.3.1
-rbenv global 2.3.1
+rbenv install 2.5.0
+rbenv global 2.5.0
 
 echo "Installing setting up Node, this could take a while..."
-nodenv install 6.3.1
-nodenv global 6.3.1
+nodenv install 6.12.3
+nodenv global 6.12.3
